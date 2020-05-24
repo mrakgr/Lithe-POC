d17c5cf51e5314cc7e623b9a21f1bfb9648aee12
Marko Grdinić <mrakgr@gmail.com>
2020-05-24 17:05:03 +0200

    No, Hopac is not going for me. The ulterior motive behind me trying it out is to see whether streams could be used on a editor support problem for Spiral, but after mulling it over, I've come to the conclussion that using reactive combinators and propagating lazy values is superior to lazy streams.
    I need switching behavior for the compiler pipeline and lazy streams are not good for this purpose.
    
    Though I have desire to master it, I am having real difficulty finding where they would be better than Rx. UIs or editor support aren't it. Rx is tough competition.

--- Lithe Avalonia Examples/Main.fs
+++ Lithe Avalonia Examples/Main.fs
@@ -15,6 +15,4 @@ open NetMQ
 open NetMQ.Sockets
 
 [<EntryPoint>]
-let main argv = 
-    //Avalonia.ZeroMQ.Main.main argv
-    Avalonia.StackButtons.Main.main argv
+let main argv = Avalonia.ZeroMQ.Main.main argv
